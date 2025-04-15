5ab456510e7bd4c850e42cfb53fef005ce8cd941
mkslanc <mkslanc@gmail.com>
2025-04-15 20:59:55 +0400

    createPatch

--- diff0/diff-test.js
+++ diff0/diff-test.js
@@ -13,71 +13,11 @@ const { diff_match_patch, DIFF_DELETE, DIFF_INSERT, DIFF_EQUAL } = require("./di
  * @returns {string} - The Git diff-like patch.
  */
 function generateGitDiffDMP(oldText, newText, fileName) {
-    const dmp = new diff_match_patch();
-    dmp.Diff_Timeout = 0;
-    const diffs = dmp.diff_main(oldText, newText, false);
-    dmp.diff_cleanupSemantic(diffs);
-
-    const patchLines = [];
-    if (diffs.length > 0) {
-
-
-        let oldLine = 1;
-        let newLine = 1;
-        let hunkHeaderAdded = false;
-        let hunkLines = [];
-
-        diffs.forEach(([type, text]) => {
-            const lines = text.split("\n");
-
-            /*if (type === DIFF_EQUAL) {
-                lines.forEach((line, index) => {
-                    if (line && !hunkHeaderAdded) {
-                        patchLines.push(`@@ -${oldLine},${lines.length} +${newLine},${lines.length} @@`);
-                        hunkHeaderAdded = true;
-                    }
-                    if (line) {
-                        hunkLines.push(` ${line}`);
-                        oldLine++;
-                        newLine++;
-                    }
-                });
-            } else */if (type === DIFF_DELETE) {
-                lines.forEach((line, index) => {
-                    if (line) {
-                        if (!hunkHeaderAdded) {
-                            patchLines.push(`@@ -${oldLine},${lines.length} +${newLine},${lines.length} @@`);
-                            hunkHeaderAdded = true;
-                        }
-                        hunkLines.push(`-${line}`);
-                        oldLine++;
-                    }
-                });
-            } else if (type === DIFF_INSERT) {
-                lines.forEach((line, index) => {
-                    if (line) {
-                        if (!hunkHeaderAdded) {
-                            patchLines.push(`@@ -${oldLine},${lines.length} +${newLine},${lines.length} @@`);
-                            hunkHeaderAdded = true;
-                        }
-                        hunkLines.push(`+${line}`);
-                        newLine++;
-                    }
-                });
-            }
-        });
-
-        if (hunkLines.length > 0) {
-            patchLines.push(`diff --git a/${fileName} b/${fileName}`);
-            patchLines.push(`--- a/${fileName}`);
-            patchLines.push(`+++ b/${fileName}`);
-            patchLines.push(...hunkLines);
-        }
-    }
-
-    return patchLines.join("\n");
+    const oldLines = oldText.split(/\r?\n/);
+    const newLines = newText.split(/\r?\n/);
+    var changes = diffLines(oldLines, newLines);
+    return createPatch(oldLines, newLines, changes, fileName);
 }
-
 
 const { computeDiff } = require("./vscode-diff/index");
 
@@ -95,93 +35,7 @@ function generateGitDiffFromComputeDiff(oldText, newText, fileName) {
     // Call computeDiff to get the changes
     const changes = computeDiff(oldLines, newLines, { ignoreTrimWhitespace: true, maxComputationTimeMs: 0 });
 
-    const patchLines = [];
-    if (changes.length > 0) {
-        patchLines.push(`diff --git a/${fileName} b/${fileName}`);
-        patchLines.push(`--- a/${fileName}`);
-        patchLines.push(`+++ b/${fileName}`);
-
-        changes.forEach(change => {
-            const { origStart, origEnd, editStart, editEnd, charChanges } = change;
-
-            // Add hunk header
-            const oldRange = `${origStart + 1},${origEnd - origStart}`;
-            const newRange = `${editStart + 1},${editEnd - editStart}`;
-            patchLines.push(`@@ -${oldRange} +${newRange} @@`);
-
-            // Add line changes
-            let oldLineIndex = origStart;
-            let newLineIndex = editStart;
-
-            charChanges && charChanges.forEach(charChange => {
-                while (oldLineIndex < charChange.originalStartLineNumber) {
-                    patchLines.push(` ${oldLines[oldLineIndex]}`);
-                    oldLineIndex++;
-                    newLineIndex++;
-                }
-
-                if (charChange.originalStartLineNumber === charChange.originalEndLineNumber) {
-                    // Handle deletions
-                    for (let i = charChange.originalStartColumn; i < charChange.originalEndColumn; i++) {
-                        patchLines.push(`-${oldLines[charChange.originalStartLineNumber].slice(i)}`);
-                    }
-                }
-
-                if (charChange.modifiedStartLineNumber === charChange.modifiedEndLineNumber) {
-                    // Handle insertions
-                    for (let i = charChange.modifiedStartColumn; i < charChange.modifiedEndColumn; i++) {
-                        patchLines.push(`+${newLines[charChange.modifiedStartLineNumber].slice(i)}`);
-                    }
-                }
-
-                oldLineIndex = charChange.originalEndLineNumber;
-                newLineIndex = charChange.modifiedEndLineNumber;
-            });
-
-            // Add remaining unchanged lines
-            while (oldLineIndex < origEnd) {
-                patchLines.push(` ${oldLines[oldLineIndex]}`);
-                oldLineIndex++;
-                newLineIndex++;
-            }
-        });
-
-    }
-
-    return patchLines.join("\n");
-}
-
-// Example usage
-// const oldText = `This is the original text.
-// It has multiple lines.
-// Some lines will be removed.`;
-
-// const newText = `This is the modified text.
-// It has multiple lines.
-// Some lines will be added.`;
-
-// const fileName = "example.txt";
-// const gitDiff = generateGitDiffFromComputeDiff(oldText, newText, fileName);
-// console.log(gitDiff);
-
-// // Example usage
-// const oldText = `This is the original text.
-// It has multiple lines.
-// Some lines will be removed.`;
-
-// const newText = `This is the modified text.
-// It has multiple lines.
-// Some lines will be added.`;
-
-// const fileName = "example.txt";
-// const gitDiff = generateGitDiff(oldText, newText, fileName);
-// console.log(gitDiff);
-
-// External diff function provided by the user
-function userDiff(oldText, newText) {
-    // Example implementation of a diff function (replace with the actual user-provided function)
-    const jsdiff = require("diff");
-    return jsdiff.createPatch("file", oldText, newText);
+    return createPatch(oldLines, newLines, changes, fileName);
 }
 
 var userDiffs = [generateGitDiffDMP, generateGitDiffFromComputeDiff];
@@ -260,6 +114,234 @@ function compareDiffs() {
     });
 
     console.log("Diff comparison completed.");
+}
+
+// part for diff_match_patch.js
+var diff_linesToChars_ = function(text1, text2) {
+    var lineHash = Object.create(null);
+    var lineCount = 1;
+
+    function diff_linesToCharsMunge_(lines) {
+        var chars = "";
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i].trim();
+            if (typeof lineHash[line] === "number") {
+                chars += String.fromCharCode(lineHash[line]);
+            } else {
+                chars += String.fromCharCode(lineCount);
+                lineHash[line] = lineCount++;
+            }
+        }
+        return chars;
+    }
+    var chars1 = diff_linesToCharsMunge_(text1);
+    var chars2 = diff_linesToCharsMunge_(text2);
+    return {
+        chars1: chars1,
+        chars2: chars2,
+    };
+};
+
+function diffLines(oldLines, newLines) {
+    const dmp = new diff_match_patch();
+    var a = diff_linesToChars_(oldLines, newLines);
+    var diff = dmp.diff_main(a.chars1, a.chars2, false);
+    var chunks = [];
+    var offset = {
+        left: 0,
+        right: 0,
+    };
+    var lastChunk;
+    diff.forEach(function(chunk) {
+        var chunkType = chunk[0];
+        var length = chunk[1].length;
+
+        // oddly, occasionally the algorithm returns a diff with no changes made
+        if (length === 0) {
+            return;
+        }
+        if (chunkType === 0) {
+            offset.left += length;
+            offset.right += length;
+            lastChunk = null;
+        } else if (chunkType === -1) {
+            if (lastChunk) {
+                lastChunk.origEnd = Math.max(offset.left + length, lastChunk.origEnd);
+                lastChunk.editEnd = Math.max(offset.right, lastChunk.editEnd);
+            } else {
+                chunks.push(
+                    (lastChunk = {
+                        origStart: offset.left,
+                        origEnd: offset.left + length,
+                        editStart: offset.right,
+                        editEnd: offset.right,
+                    })
+                );
+            }
+            offset.left += length;
+        } else if (chunkType === 1) {
+            if (lastChunk) {
+                lastChunk.origEnd = Math.max(offset.left, lastChunk.origEnd);
+                lastChunk.editEnd = Math.max(offset.right + length, lastChunk.editEnd);
+            } else {
+                chunks.push(
+                    (lastChunk = {
+                        origStart: offset.left,
+                        origEnd: offset.left,
+                        editStart: offset.right,
+                        editEnd: offset.right + length,
+                    })
+                );
+            }
+            offset.right += length;
+        }
+    });
+
+    chunks.forEach(function(diff) {
+        var inlineChanges = [];
+        var type = 0;
+        if (diff.origStart == diff.origEnd) {
+            type = 1;
+        } else if (diff.editStart == diff.editEnd) {
+            type = -1;
+        } else {
+            var inlineDiff = dmp.diff_main(
+                oldLines.slice(diff.origStart, diff.origEnd).join("\n"),
+                newLines.slice(diff.editStart, diff.editEnd).join("\n"),
+                false
+            );
+            dmp.diff_cleanupSemantic(inlineDiff);
+            inlineDiff.forEach(function(change) {
+                var text = change[1];
+                var lines = text.split("\n");
+                var rowCh = lines.length - 1;
+                var colCh = lines[rowCh].length;
+                var changeType = change[0];
+                if (text.length) {
+                    inlineChanges.push([changeType, rowCh, colCh]);
+                    // if (changeType) {
+                    //     if (!type) {
+                    //         type = changeType;
+                    //     } else if (type != changeType) {
+                    //         type = 2;
+                    //     }
+                    // }
+                }
+            });
+        }
+        diff.inlineChanges = inlineChanges;
+        diff.type = type;
+    });
+    return chunks;
+}
+
+function createPatch(oldLines, newLines, changes, fileName) {
+    var chunks = changes;
+    var editLines = newLines;
+    var origLines = oldLines;
+    var path1 = fileName;
+    var path2 = fileName;
+    var patch = [
+        "diff --git a/" + path1 + " b/" + path2,
+        "--- a/" + path1,
+        "+++ b/" + path2,
+    ].join("\n");
+
+    if (!chunks.length) {
+        chunks = [
+            {
+                origStart: 0,
+                origEnd: 0,
+                editStart: 0,
+                editEnd: 0,
+            },
+        ];
+    }
+
+    function header(s1, c1, s2, c2) {
+        return (
+            "@@ -" +
+            (c1 ? s1 + 1 : s1) +
+            "," +
+            c1 +
+            " +" +
+            (c2 ? s2 + 1 : s2) +
+            "," +
+            c2 +
+            " @@"
+        );
+    }
+
+    var context = 0;
+    // changed newline at the end of file
+    var editEOF = !editLines[editLines.length - 1];
+    var origEOF = !origLines[origLines.length - 1];
+    if (editEOF) editLines.pop();
+    if (origEOF) origLines.pop();
+    if (editEOF != origEOF) {
+        chunks = chunks.slice();
+        var last = chunks.pop();
+        chunks.push(
+            (last = {
+                origStart: Math.min(last.origStart, origLines.length - 1),
+                origEnd: Math.min(last.origEnd, origLines.length),
+                editStart: Math.min(last.editStart, editLines.length - 1),
+                editEnd: Math.min(last.editEnd, editLines.length),
+            })
+        );
+    }
+
+    var hunk = "";
+    var start1 = 0;
+    var start2 = 0;
+    var end1 = 0;
+    var end2 = 0;
+    var length1 = 0;
+    var length2 = 0;
+    var mergeWithNext = false;
+    for (var i = 0; i < chunks.length; i++) {
+        var ch = chunks[i];
+        var s1 = ch.origStart;
+        var e1 = ch.origEnd;
+        var s2 = ch.editStart;
+        var e2 = ch.editEnd;
+        var next = chunks[i + 1];
+
+        start1 = Math.max(s1 - context, end1);
+        start2 = Math.max(s2 - context, end2);
+        end1 = Math.min(e1 + context, origLines.length);
+        end2 = Math.min(e2 + context, editLines.length);
+
+        mergeWithNext = false;
+        if (next) {
+            if (end1 >= next.origStart - context) {
+                end1 = next.origStart;
+                end2 = next.editStart;
+                mergeWithNext = true;
+            }
+        }
+
+        for (var j = start1; j < s1; j++) hunk += "\n " + origLines[j];
+        for (var j = s1; j < e1; j++) hunk += "\n-" + origLines[j];
+        if (ch == last && editEOF) hunk += "\n\\ No newline at end of file";
+        for (var j = s2; j < e2; j++) hunk += "\n+" + editLines[j];
+        if (ch == last && origEOF) hunk += "\n\\ No newline at end of file";
+        for (var j = e1; j < end1; j++) hunk += "\n " + origLines[j];
+
+        length1 += end1 - start1;
+        length2 += end2 - start2;
+        if (mergeWithNext) continue;
+
+        patch += "\n" + header(end1 - length1, length1, end2 - length2, length2) + hunk;
+        length2 = length1 = 0;
+        hunk = "";
+    }
+
+    if (!editEOF && !origEOF && end1 == origLines.length) {
+        patch += "\n\\ No newline at end of file";
+    }
+
+    return patch;
 }
 
 // Run the main function
