79a54c0160e2f6b99850fa3fbb6029958875105e
mkslanc <mkslanc@gmail.com>
2025-04-21 19:52:51 +0400

    move scroll decorators; disable scroll decorators for now

--- src/ext/diff/ace_diff.js
+++ src/ext/diff/ace_diff.js
@@ -79,7 +79,7 @@ class DiffHighlight {
     static MAX_RANGES = 500;
 
     update(html, markerLayer, session, config) {
-        let side, dir, operation, opOperation;
+        let dir, operation, opOperation;
         var diffView = this.diffView;
         if (this.type === -1) {// original editor
             dir = "old";
@@ -123,13 +123,10 @@ class DiffHighlight {
             })
         }
 
-        //editor.renderer.$scrollDecorator.zones = [];
         lineChanges.forEach((lineChange) => {
             let startRow = lineChange[dir].start.row;
             let endRow = lineChange[dir].end.row;
             let range = new Range(startRow, 0, endRow - 1, 1 << 30);
-            // TODO: why does this throw an error?
-            // editor.renderer.$scrollDecorator.addZone(range.start.row, range.end.row, operation);
             if (startRow !== endRow) {
                 range = range.toScreenRange(session);
                 markerLayer.drawFullLineMarker(html, range, "ace_diff " + operation + " inline", config);
@@ -198,8 +195,6 @@ class DiffHighlight {
                 }
             }
         });
-        //TODO: hack for decorators to be forcely updated until we got new change type in VirtualRenderer
-        //editor.renderer.$scrollDecorator.$updateDecorators(config);
     }
 }
 
